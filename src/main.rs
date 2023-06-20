0d703d5699bbd9d874688caa6b5b0496cc584c94
Pream Pinbut <momozahara@hotmail.com>
2023-06-20 16:51:31 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+
+fn main() {
+    // Read the command-line argument for the message
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please privide a message.");
+        return;
+    }
+    let contents = &args[1];
+
+    // Count words and characters
+    let word_count = contents.split_whitespace().count();
+    let char_count = contents.chars().count();
+    let char_nwhitespace_count = contents.replace(" ", "").chars().count();
+
+    // Print the results
+    println!("Word count: {word_count}");
+    println!("Character count: {char_count}");
+    println!("Character no space count: {char_nwhitespace_count}");
+}
