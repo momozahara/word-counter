c07b1211f66970e0a0cb7a0cef3fcc2093bd7ecb
Pream Pinbut <momozahara@hotmail.com>
2023-07-06 20:15:31 +0700

    add line count

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ this is my own version of wc for personal use
 
 ### Usage
 
+word count
 ```bash
 wcc "hello world"
+```
+
+line count
+```bash
+wcc -l "hello\n world"
+```
+
+```bash
+wcc -l "hello
+world"
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 use std::env;
 
-fn main() {
-    // Read the command-line argument for the message
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Please provide a message.");
-        return;
-    }
-
-    // Skip the first argument, which is the program's name
+fn normal_run(args: Vec<String>) {
     let mut result = String::new();
     for arg in args.iter().skip(1) {
         result.push_str(arg.replace('\n', " ").as_str());
@@ -17,14 +9,38 @@ fn main() {
 
     result = result.trim().to_string();
 
-    // Count words and characters
     let word_count = result.split_whitespace().count();
     let char_count = result.replace(" ", "").chars().count();
     let char_count_withspace = result.chars().count();
 
-    // Print the results
     println!("Echo: {result}");
     println!("Word count: {word_count}");
     println!("Character count: {char_count}");
     println!("Character count with space: {char_count_withspace}");
+}
+
+fn line_run(args: Vec<String>) {
+    let mut result = String::new();
+    for arg in args.iter().skip(1) {
+        result.push_str(arg.as_str());
+    }
+
+    let line_count = result.lines().count();
+
+    println!("Line count: {line_count}");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please provide a message.");
+        return;
+    }
+
+    let line_flag = args.iter().find(|a| a.as_str() == "-l");
+
+    match line_flag {
+        Some(_) => line_run(args),
+        None => normal_run(args),
+    }
 }
